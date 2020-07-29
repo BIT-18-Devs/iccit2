ba189761cab6b67bee17afc2daf585e598a01840
sank2000 <santhoshvelr@yahoo.com>
2020-07-29 16:50:10 +0530

    nav bar with bugs

--- js/nav.js
+++ js/nav.js
@@ -5,3 +5,23 @@ window.onscroll = (event) => {
     document.querySelector('nav.nav').className = 'nav';
   }
 }
+
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav__items");
+
+  burger.addEventListener("click", () => {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+      nav.classList.remove("nav-active");
+      nav.classList.toggle("nav-active2");
+    } else {
+      nav.classList.remove("nav-active2");
+      nav.classList.toggle("nav-active");
+    }
+    document.querySelector(".burger__line").classList.toggle("burger__line--toggle");
+  });
+};
+
+
+navSlide();
